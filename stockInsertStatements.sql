af2b58e27b417019c2dd8227c064d1237afbb8ae
toastman716 <9998292@student.swin.edu.au>
2017-04-05 22:22:19 +1000

    Add files via upload

---
+++ stockInsertStatements.sql
@@ -0,0 +1,77 @@
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (373645325526, "Bellamy's Organic Step 1", 28.75, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (473645325526, "Colgate Toothbrush Twin Pack", 6.50, 10);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (573645325526, "Bellamy's Organic Step 2", 28.75, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (673645325526, "Bellamy's Organic Step 3", 28.75, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (773645325526, "Nappies", 20, 22);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (873645325526, "Bandaids 12 pack", 3.75, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (973645325526, "Bandaids 50 pack", 5.75, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (103645325526, "Bandaids 100 pack", 10.00, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (642735325526, "Bandages", 28.75, 8.00, 15);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (243645325526, "First Aid kit", 25.00, 16);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (273648925526, "Rubbing Alcohol", 8.00, 24);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (243645325526, "Heat Pack", 12.00, 8);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (433645325526, "Linx Deoderant", 6.25, 32);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (113645325526, "Dove Moisturiser", 6.00, 19);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (993645325526, "Redwin Moisturiser", 5.75, 13);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (813645325526, "Gillete Mens Razer", 9.00, 7);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (623645325526, "Gillete Womens Razer", 10.00, 12);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (773645325526, "Cough Syrup", 14.95, 17);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (013645325526, "Asprin 24 Pack", 4.00, 30);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (073645325526, "Disprin 24 Pack", 5.00, 15);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (123645325526, "Glasses", 30.00, 10);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (723645325526, "Wheelchair", 200.00, 5);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (427595325526, "Walking Sticks", 80.00, 10);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (510845325526, "Krutches", 125.00, 20);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (873645625526, "Mentos", 1.75, 36);
+
+INSERT INTO in_stock (code, name, price, quantity)
+VALUES (203645325526, "Pregnancy Test", 4.00, 12);
